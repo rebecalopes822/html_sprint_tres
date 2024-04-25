f945634de64adf8ad455e9f7b57ea2a27c722fdf
GiiGiantomaso <rm553369@fiap.com.br>
2024-04-25 01:41:20 -0300

    pasta lib criada e layout atualizado

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
+import StyledComponentsRegistry from "@/lib/registry";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "@/components/Navbar";
+import { Footer } from "@/components/Footer";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +20,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <StyledComponentsRegistry>
+          <Navbar />
+          {children}
+          <Footer />
+        </StyledComponentsRegistry>
+      </body>
     </html>
   );
 }
