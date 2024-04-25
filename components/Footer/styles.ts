8a08012bbd37c3ec84152975f634f56b837417b2
rebecalopes822 <rebecalopes822@gmail.com>
2024-04-25 01:29:22 -0300

    add conteúdo navbar e footer

---
+++ components/Footer/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Line = styled.div`
+  display: flex;
+  justify-content: center;
+  background-color: #00a3ff;
+  height: 50px;
+  gap: 10px;
+  align-items: center;
+`;
+
+export const Ink = styled.text``;
